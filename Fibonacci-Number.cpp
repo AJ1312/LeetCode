98c5801b88e67145bd5a1dd510c9656c155c8ef1
Ajitesh Sharma <92087813+AJ1312@users.noreply.github.com>
2025-08-01 22:35:41 +0530

    [509] [Time Beats: 77.64%] [Memory Beats: 64.11%] - LeetPush

--- Fibonacci-Number.cpp
+++ Fibonacci-Number.cpp
@@ -4,12 +4,10 @@ public:
         int a =0;
         int x =0;
         int y=1;
-        if(n==0){
-            return 0;
+        if(n==0 || n==1){
+            return n;
         }
-        if(n==1){
-            return 1;
-        }
+       
         for(int i=1; i<n;++i){
             a = x+y;
             x=y;
