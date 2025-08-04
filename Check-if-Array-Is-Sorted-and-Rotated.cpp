4dd2e7c7600381762d31b406652bcb91421da7ea
Ajitesh Sharma <92087813+AJ1312@users.noreply.github.com>
2025-08-05 00:06:57 +0530

    [1752] [Time Beats: 100.00%] [Memory Beats: 75.43%] - LeetPush

--- Check-if-Array-Is-Sorted-and-Rotated.cpp
+++ Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -1,21 +1,34 @@
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//     vector<int> nums2 = nums;
+//     sort(nums2.begin(), nums2.end());
+//     for(int i=0; i<nums.size(); i++){
+//         if(nums==nums2){
+//             return true;
+//             break;
+//         }else{
+//       for(int i=1; i< nums.size(); i++){
+//             int temp = nums[i-1];
+//             nums[i-1] = nums[i];
+//             nums[i] = temp;
+//       }
+//     }
+//     }
+
+//         return false;
+//     }
+// };
 class Solution {
 public:
     bool check(vector<int>& nums) {
-    vector<int> nums2 = nums;
-    sort(nums2.begin(), nums2.end());
-    for(int i=0; i<nums.size(); i++){
-        if(nums==nums2){
-            return true;
-            break;
-        }else{
-      for(int i=1; i< nums.size(); i++){
-            int temp = nums[i-1];
-            nums[i-1] = nums[i];
-            nums[i] = temp;
-      }
-    }
-    }
-
-        return false;
+        int n = nums.size(), drops = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                drops++;
+                if (drops > 1) return false;
+            }
+        }
+        return true;
     }
 };
