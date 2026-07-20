9e7f3f44e2834b2cad6f57f237f8d0f58d6f4589
Ajitesh Sharma <92087813+AJ1312@users.noreply.github.com>
2026-07-20 23:59:20 +0530

    Time: 1 ms (99.96%), Space: 77.2 MB (66.26%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,13 +3,14 @@ class Solution {
         int be = nums[0];
         int ans = nums[0];
         int n = nums.length;
+        int v1 = 0;
+        int v2 = 0;
+
 
 
         for(int i=1;i<n;i++){
-            int v1= be + nums[i];
-            int v2= nums[i];
-
-
+            v1 = be + nums[i];
+            v2 = nums[i];
             be = Math.max(v1, v2);
             ans = Math.max(ans, be);
         }
